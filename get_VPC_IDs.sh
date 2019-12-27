960842521fb2c77297210d9b11ef75aaf5b7d986
csmunuku <csmunuku@gmail.com>
2019-12-27 07:57:56 -0600

    Update get_VPC_IDs.sh

--- get_VPC_IDs.sh
+++ get_VPC_IDs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#####################################################################################
+#########################################################################################
 # AUTHOR: Chandra Munukutla
 # DESC: Get VPC ID (other than the default VPC) in an aws account.
 #       Use aws profile information for accessing the account.
-#####################################################################################
+#       based on convetion used, this can be adjusted.. here the convention for vpc name
+#       has been given as <ENV>-vpc
+#########################################################################################
 
 source ./aws_ask_file
 ask_profile
 ask_env
 
-aws ec2 describe-vpcs --filters "Name=tag:Name,Values=*${MY_ENV}-vpc" --region us-east-1 ${PRO} | jq -cr '.Vpcs[].VpcId'
+aws ec2 describe-vpcs \
+        --filters "Name=tag:Name,Values=*${MY_ENV}-vpc" \
+        --region us-east-1 ${PRO} | jq -cr '.Vpcs[].VpcId'
